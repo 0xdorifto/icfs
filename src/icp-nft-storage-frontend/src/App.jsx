825475e9d67fb81a8a6eaf16e2489b89d87259ab
0xdorifto <gastaosousafaria@gmail.com>
2024-07-04 00:09:50 +0100

    Change to rust

--- src/icp-nft-storage-frontend/src/App.jsx
+++ src/icp-nft-storage-frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import { icp_nft_storage_backend } from 'declarations/icp-nft-storage-backend';
+import { useState } from "react";
+import { icp_nft_storage_backend } from "declarations/icp-nft-storage-backend";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [greeting, setGreeting] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -19,7 +19,7 @@ function App() {
       <br />
       <br />
       <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <label htmlFor="name">Hello: &nbsp;</label>
         <input id="name" alt="Name" type="text" />
         <button type="submit">Click Me!</button>
       </form>
