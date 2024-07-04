af3becd71e51ed2b43f1a17454758cf3f37865df
0xdorifto <gastaosousafaria@gmail.com>
2024-07-04 23:31:18 +0100

    Add fields to collection

--- src/collection/src/lib.rs
+++ src/collection/src/lib.rs
@@ -1,26 +1,28 @@
 use candid::{CandidType, Deserialize, Principal};
 use ic_cdk;
-// use serde_json;
+use serde::Serialize;
 use std::cell::RefCell;
 use std::collections::HashMap;
 
-#[derive(CandidType, Deserialize, Clone)]
+#[derive(CandidType, Deserialize, Serialize, Clone)]
 struct Attribute {
     trait_type: String,
     value: String,
 }
 
-#[derive(CandidType, Deserialize, Clone)]
+#[derive(CandidType, Deserialize, Serialize, Clone)]
 struct Metadata {
     name: String,
     description: String,
     image: String,
     attributes: Vec<Attribute>,
+    external_url: String,
 }
 
 #[derive(CandidType, Deserialize, Clone)]
 struct CollectionInfo {
     owner: Principal,
+    name: String,
     collection_size: u64,
     chain_name: String,
     description: String,
@@ -31,6 +33,7 @@ thread_local! {
     static METADATA_STORE: RefCell<HashMap<u64, Metadata>> = RefCell::new(HashMap::new());
     static COLLECTION_INFO: RefCell<CollectionInfo> = RefCell::new(CollectionInfo {
         owner: Principal::anonymous(),
+        name: String::new(),
         collection_size: 0,
         chain_name: String::new(),
         description: String::new(),
@@ -38,10 +41,33 @@ thread_local! {
     });
 }
 
+// init
+#[ic_cdk::init]
+fn init(info: CollectionInfo) {
+    COLLECTION_INFO.with(|ci| *ci.borrow_mut() = info);
+}
+
+// modifiers
+fn is_owner() -> Result<(), String> {
+    let caller = ic_cdk::caller();
+    COLLECTION_INFO.with(|info| {
+        if info.borrow().owner == caller {
+            Ok(())
+        } else {
+            Err("Only the owner can perform this action".to_string())
+        }
+    })
+}
+
 // Collection Info Getters
 #[ic_cdk::query]
 fn get_owner() -> Principal {
     COLLECTION_INFO.with(|info| info.borrow().owner)
+}
+
+#[ic_cdk::query]
+fn get_name() -> String {
+    COLLECTION_INFO.with(|info| info.borrow().name.clone())
 }
 
 #[ic_cdk::query]
@@ -70,21 +96,17 @@ fn get_all_collection_info() -> CollectionInfo {
 }
 
 // Collection Info Setters
-fn is_owner() -> Result<(), String> {
-    let caller = ic_cdk::caller();
-    COLLECTION_INFO.with(|info| {
-        if info.borrow().owner == caller {
-            Ok(())
-        } else {
-            Err("Only the owner can perform this action".to_string())
-        }
-    })
-}
-
 #[ic_cdk::update]
 fn set_owner(owner: Principal) -> Result<(), String> {
     is_owner()?;
     COLLECTION_INFO.with(|info| info.borrow_mut().owner = owner);
+    Ok(())
+}
+
+#[ic_cdk::update]
+fn set_name(name: String) -> Result<(), String> {
+    is_owner()?;
+    COLLECTION_INFO.with(|info| info.borrow_mut().name = name);
     Ok(())
 }
 
@@ -123,6 +145,7 @@ fn set_all_collection_info(info: CollectionInfo) -> Result<(), String> {
     Ok(())
 }
 
+// functions
 #[ic_cdk::query]
 fn get_metadata(token_id: u64) -> Option<Metadata> {
     METADATA_STORE.with(|store| store.borrow().get(&token_id).cloned())
@@ -150,28 +173,28 @@ fn get_all_metadata(start: u64, limit: u64) -> Vec<(u64, Metadata)> {
     })
 }
 
-// #[ic_cdk::query]
-// fn http_request(request: HttpRequest) -> HttpResponse {
-//     let path: Vec<&str> = request.url.split('/').collect();
+#[ic_cdk::query]
+fn http_request(request: HttpRequest) -> HttpResponse {
+    let path: Vec<&str> = request.url.split('/').collect();
 
-//     if path.len() == 2 && path[0] == "metadata" {
-//         if let Ok(token_id) = path[1].parse::<u64>() {
-//             if let Some(metadata) = get_metadata(token_id) {
-//                 HttpResponse {
-//                     status_code: 200,
-//                     headers: vec![("Content-Type".to_string(), "application/json".to_string())],
-//                     body: serde_json::to_vec(&metadata).unwrap(),
-//                 }
-//             } else {
-//                 not_found()
-//             }
-//         } else {
-//             bad_request()
-//         }
-//     } else {
-//         not_found()
-//     }
-// }
+    if path.len() == 2 && path[0] == "metadata" {
+        if let Ok(token_id) = path[1].parse::<u64>() {
+            if let Some(metadata) = get_metadata(token_id) {
+                HttpResponse {
+                    status_code: 200,
+                    headers: vec![("Content-Type".to_string(), "application/json".to_string())],
+                    body: serde_json::to_vec(&metadata).unwrap(),
+                }
+            } else {
+                not_found()
+            }
+        } else {
+            bad_request()
+        }
+    } else {
+        not_found()
+    }
+}
 
 #[derive(CandidType, Deserialize)]
 struct HttpRequest {
@@ -181,27 +204,27 @@ struct HttpRequest {
     headers: Vec<(String, String)>,
 }
 
-#[derive(CandidType, Deserialize)]
+#[derive(CandidType, Serialize)]
 struct HttpResponse {
     status_code: u16,
     headers: Vec<(String, String)>,
     body: Vec<u8>,
 }
 
-// fn not_found() -> HttpResponse {
-//     HttpResponse {
-//         status_code: 404,
-//         headers: vec![],
-//         body: b"Not Found".to_vec(),
-//     }
-// }
+fn not_found() -> HttpResponse {
+    HttpResponse {
+        status_code: 404,
+        headers: vec![],
+        body: b"Not Found".to_vec(),
+    }
+}
 
-// fn bad_request() -> HttpResponse {
-//     HttpResponse {
-//         status_code: 400,
-//         headers: vec![],
-//         body: b"Bad Request".to_vec(),
-//     }
-// }
+fn bad_request() -> HttpResponse {
+    HttpResponse {
+        status_code: 400,
+        headers: vec![],
+        body: b"Bad Request".to_vec(),
+    }
+}
 
 ic_cdk::export_candid!();
